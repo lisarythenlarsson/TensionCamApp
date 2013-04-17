4a375f6133de3c0efcb68494ad3f3e74c0fb4167
Fredrik Johansson <c.f.johansson@hotmail.com>
2013-04-17 15:47:11 +0200

    Created preview-class and code for accessing the camera

--- Project folder/src/com/example/tensionnsioncamapp_project/MainActivity.java
+++ Project folder/src/com/example/tensionnsioncamapp_project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tensionnsioncamapp_project;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -18,5 +19,19 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	// Getting an instance of the Camera object.
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	    	e.printStackTrace();
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
 
+	
 }
