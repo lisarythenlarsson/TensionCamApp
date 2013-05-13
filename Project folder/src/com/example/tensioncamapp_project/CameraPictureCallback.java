d9b5ab71b6bc01bf38318c72d3a36a8410d54c5d
Lisa Rythén Larsson <lisarythenlarsson@dhcp-184025.eduroam.chalmers.se>
2013-05-13 14:38:05 +0200

    created an directory and albumname called TensionCamApp

--- Project folder/src/com/example/tensioncamapp_project/CameraPictureCallback.java
+++ Project folder/src/com/example/tensioncamapp_project/CameraPictureCallback.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
+import android.os.Environment;
 import android.util.Log;
 
 public class CameraPictureCallback {
@@ -22,7 +25,7 @@ public class CameraPictureCallback {
 
     			File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
     			if (pictureFile == null){
-    				Log.d(TAG, "Error creating media file, check storage permissions: ");
+    				Log.d(TAG, "Error creating media file, check storage permissions");
     				return;
     			}
 
@@ -36,11 +39,44 @@ public class CameraPictureCallback {
     				Log.d(TAG, "Error accessing file: " + e.getMessage());
     			}
     		}
-
+    		
+    		/** Create a File for saving an image or video */
 			private File getOutputMediaFile(int mediaTypeImage) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-    	};  
+				if (!isExternalStorageWritable()){
+					Log.d(TAG,"Can't access the extrenal storage");
+					return null;
+				}
+					File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+			    		Environment.DIRECTORY_PICTURES), "TensionCamApp");
+				    	// Creating the storage directory if it does not exist
+				    	if (! mediaStorageDir.exists()){
+				    		if (! mediaStorageDir.mkdirs()){
+				    			Log.d("TensionCamApp", "failed to create directory");
+				    			return null;
+				    		}
+				    	}
+
+				    	// Create a media file name, might want to include location
+				    	String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date(0));
+				    	File mediaFile;
+				    	if (mediaTypeImage == MEDIA_TYPE_IMAGE){
+				    		mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+				    				"IMG_"+ timeStamp + ".jpg");
+				    	} else {
+				    		return null;
+				    	}
+
+				    	return mediaFile;
+				}
+			}; 
+		
+    	}
+		/**checking if external storage is read and writable */
+		public static boolean isExternalStorageWritable() {
+    		    String state = Environment.getExternalStorageState();
+    		    if (Environment.MEDIA_MOUNTED.equals(state)) {
+    		        return true;
+    		    }
+    		    return false;
     }
 }
