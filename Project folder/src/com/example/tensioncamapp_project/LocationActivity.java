d1a9c29d40e4d5dea8d8df3997634d75d4f5dac4
Lisa Rythén Larsson <lisarythenlarsson@dhcp-181085.eduroam.chalmers.se>
2013-05-14 11:34:07 +0200

    added setListAdapter()

--- Project folder/src/com/example/tensioncamapp_project/LocationActivity.java
+++ Project folder/src/com/example/tensioncamapp_project/LocationActivity.java
@@ -3,22 +3,27 @@ package com.example.tensioncamapp_project;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class LocationActivity extends ListActivity {
 	
 	String location[] = {"choose a location", "BigGlen", "MiddleGlen", "SmallGlen"};
 	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(LocationActivity.this,R.layout.activity_location, this.location));
+	}
+	
+	
+	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		// TODO Auto-generated method stub
 		super.onListItemClick(l, v, position, id);
 	}
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-	}
+	
 
 	
 }
