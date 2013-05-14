fdb88b408f9cbdd679b9c8373f06692932245f65
Lisa Rythén Larsson <lisarythenlarsson@dhcp-181085.eduroam.chalmers.se>
2013-05-14 11:21:06 +0200

    created class for scrollView

---
+++ Project folder/src/com/example/tensioncamapp_project/LocationActivity.java
@@ -0,0 +1,24 @@
+package com.example.tensioncamapp_project;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class LocationActivity extends ListActivity {
+	
+	String location[] = {"choose a location", "BigGlen", "MiddleGlen", "SmallGlen"};
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	
+}
