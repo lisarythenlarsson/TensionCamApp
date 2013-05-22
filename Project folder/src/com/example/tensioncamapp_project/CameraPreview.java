ded33e2c6e20690d6b880b1ce039b0662945dd07
Lisa Rythén Larsson <lisa.rythen.larsson@hotmail.com>
2013-05-22 17:59:02 +0200

    commenting

--- Project folder/src/com/example/tensioncamapp_project/CameraPreview.java
+++ Project folder/src/com/example/tensioncamapp_project/CameraPreview.java
@@ -45,41 +45,8 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-    	setCameraDisplayOrientation();
+    	//
     }
 
-    public void setCameraDisplayOrientation() 
-    {        
-         if (this.mCamera == null)
-         {
-             Log.d(TAG,"setCameraDisplayOrientation - camera null");
-             return;             
-         }
-
-         Camera.CameraInfo info = new Camera.CameraInfo();
-         Camera.getCameraInfo(0, info);
-
-         WindowManager winManager = (WindowManager) this.mContext.getSystemService(Context.WINDOW_SERVICE);
-         int rotation = winManager.getDefaultDisplay().getRotation();
-
-         int degrees = 0;
-
-         switch (rotation) 
-         {
-             case Surface.ROTATION_0: degrees = 0; break;
-             case Surface.ROTATION_90: degrees = 90; break;
-             case Surface.ROTATION_180: degrees = 180; break;
-             case Surface.ROTATION_270: degrees = 270; break;
-         }
-
-         int result;
-         /**if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) 
-         {
-             result = (info.orientation + degrees) % 360;
-             result = (360 - result) % 360;  // compensate the mirror
-         } else {  // back-facing
-            */ result = (info.orientation - degrees + 360) % 360;
-         
-         this.mCamera.setDisplayOrientation(result);
-    	}
+    
     }
