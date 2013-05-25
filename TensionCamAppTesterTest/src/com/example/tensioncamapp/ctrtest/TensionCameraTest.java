fe417739b12ebd86705d726d290373dc34ab451d
Fredrik Johansson <c.f.johansson@hotmail.com>
2013-05-25 10:49:07 +0200

    Merge to working commit

---
+++ TensionCamAppTesterTest/src/com/example/tensioncamapp/ctrtest/TensionCameraTest.java
@@ -0,0 +1,20 @@
+package com.example.tensioncamapp.ctrtest;
+
+import android.hardware.Camera;
+import android.test.AndroidTestCase;
+
+import com.example.tensioncamapp.ctr.*;
+
+public class TensionCameraTest extends AndroidTestCase {
+
+	/**
+	 * Checks that the method getCameraInstance returns an object of the type Camera 
+	 * Requires an SD-card
+	 */
+	public void testGetCameraInstance() {
+		assertTrue(TensionCamera.getCameraInstance().getClass() == Camera.class); 
+	}
+	
+	
+	
+}
