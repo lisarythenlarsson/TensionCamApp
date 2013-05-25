d5be540bed60753fa273276807487b6d84901917
maxdubois <maxd@student.chalmers.se>
2013-05-25 17:05:29 +0200

    Updated and fixed the automatic tests

---
+++ TensionCamAppTester/src/com/example/tensioncamapp/test/CameraActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.tensioncamapp.test;
+
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+
+import com.example.tensioncamapp.activity.CameraActivity;
+import com.example.tensioncamapp.activity.R;
+import com.example.tensioncamapp.utils.FileHandler;
+
+public class CameraActivityTest extends ActivityInstrumentationTestCase2<CameraActivity> {
+
+	private static CameraActivity cam;
+	
+	public CameraActivityTest () {
+		super(CameraActivity.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		setActivityInitialTouchMode(false);
+
+		cam = getActivity();
+	}
+	
+	/**Checks that a picture is being saved once the capture button is pressed*/
+	public void testCaptureButton() {
+		FileHandler.deleteFromExternalStorage(); //wipes the directory
+		TouchUtils.clickView(this, cam.findViewById(com.example.tensioncamapp.activity.R.id.button_capture_symbol)); //captures a picture
+		assertFalse(FileHandler.pathToString().isEmpty()); //checks that there is a file stores in the directory
+	}	
+
+	
+}
